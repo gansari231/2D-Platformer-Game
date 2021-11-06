c753438ebc123ba2238d0a0d847d607e0904d6f0
HP-LAPI\Gufran <ansarigufraan0@gmail.com>
2021-11-06 22:41:36 +0530

    Commit-2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator player_animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("Horizontal");
+        player_animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+    }
+}
